5269b0f01728587215aae303e313eff80c16342e
TaNuNu-Tello <alex36040@rambler.ru>
2020-10-10 17:50:32 +0300

    перед второй проверкой.

--- js/main.js
+++ js/main.js
@@ -31,11 +31,7 @@ $(document).ready(function () {
       onlyInViewport: false,
     },
   });
-  /*$(".parallax-window").parallax({ imageSrc: "../img/newletter-bg.jpeg" });
-  $(".parallax-window").parallax({
-    naturalWidth: 600,
-    naturalHeight: 400,
-  });*/
+  
   var menuButton = document.querySelector(".menu-button");
   menuButton.addEventListener("click", function () {
     //console.log("Клик по кнопке меню");
@@ -47,7 +43,7 @@ $(document).ready(function () {
   var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
-
+  
   function openModal() {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
@@ -62,6 +58,9 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+
+
+
   //Обработка форм
   $(".modal__form").validate({
     errorClass: "invalid",
@@ -100,11 +99,17 @@ $(document).ready(function () {
       },
     });
   });
-  AOS.init();
-});
+  
+  
+ $(document).ready(function() {
+    $(".phone").mask("+7 (999) 999-99-99");
+  });
 document.querySelector(".map").addEventListener("mouseenter", () => {
   if (!document.querySelector(".map iframe")) {
     document.querySelector(".map").innerHTML =
       '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.5765260121048!2d100.88038641468516!3d12.934916890879924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310296059bd3779b%3A0x37191e723a1a258!2sHilton%20Pattaya!5e0!3m2!1sru!2sru!4v1596621214582!5m2!1sru!2sru" width="100%" height="214" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
   }
+});
+AOS.init();
+  
 });
